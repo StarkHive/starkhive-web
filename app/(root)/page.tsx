94004e2f40f05cf83030877e47b8dc48cc083abb
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2025-02-26 15:12:04 +0100

    feat: add get started section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,4 @@
 import OurTechnology from "@/components/OurTechnology";
-import GetStartedSection from "@/components/ReadyToGetStarted";
 import WhyChooseUs from "@/components/ui/why-choose-us";
 import React from "react";
 
@@ -7,6 +6,5 @@ export default function Home() {
   return <div>
     <WhyChooseUs />
     <OurTechnology/>
-    <GetStartedSection/>
   </div>;
 }
