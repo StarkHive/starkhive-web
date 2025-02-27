a12013627d2769951ac33fd96b611cac06aa22f2
heojay0309 <heojay0309@gmail.com>
2025-02-27 11:39:45 -0700

    feat: hero page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,14 @@
-import OurTechnology from "@/components/OurTechnology";
-import WhyChooseUs from "@/components/ui/why-choose-us";
-import React from "react";
+import Hero from '@/components/hero';
+import OurTechnology from '@/components/OurTechnology';
+import WhyChooseUs from '@/components/ui/why-choose-us';
+import React from 'react';
 
 export default function Home() {
-  return <div>
-    <WhyChooseUs />
-    <OurTechnology/>
-  </div>;
+  return (
+    <div>
+      <Hero />
+      <WhyChooseUs />
+      <OurTechnology />
+    </div>
+  );
 }
